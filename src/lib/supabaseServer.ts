09d99896c08f1592d17ac6643deb9963dd3a8f60
impulkit <impulkit@gmail.com>
2026-02-08 02:30:33 +0530

    Update supabaseServer.ts to create a server-side Supabase client

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Initialize Supabase client
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+  },
+});
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
